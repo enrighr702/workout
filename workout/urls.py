83978aad4bfd70cce4f742196a59e11259cf1f0a
enrighr702 <ryanenright1202@gmail.com>
2026-02-23 12:47:03 +0000

    Initial Commit

---
+++ workout/urls.py
@@ -0,0 +1,9 @@
+from django_distill import distill_path
+from . import views
+
+def get_index():
+    return None
+
+urlpatterns = [
+    distill_path('', views.index, name='index', distill_func=get_index),
+]
